478a1198d0d7f8e647e50742cb7aed5832a96006
flexxy-07 <sumitrpt007@gmail.com>
2026-02-09 10:14:16 +0530

    shifted all APIs top env

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -40,47 +41,47 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCQYqJwyhsbRrfT2_0jghJNyenFVvnWqPI',
-    appId: '1:99000635271:web:497a34818bd0cc0d454d22',
-    messagingSenderId: '99000635271',
-    projectId: 'spotifyx07',
-    authDomain: 'spotifyx07.firebaseapp.com',
-    storageBucket: 'spotifyx07.firebasestorage.app',
+  static FirebaseOptions web = FirebaseOptions(
+    apiKey: dotenv.env['WEB_API_KEY'] ?? '',
+    appId: dotenv.env['WEB_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['WEB_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['WEB_PROJECT_ID'] ?? '',
+    authDomain: dotenv.env['WEB_AUTH_DOMAIN'],
+    storageBucket: dotenv.env['WEB_STORAGE_BUCKET'],
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBkhKRE9Jr58DB4iCOOqwcyocEkhpT4veE',
-    appId: '1:99000635271:android:f0a6b779772ca4f2454d22',
-    messagingSenderId: '99000635271',
-    projectId: 'spotifyx07',
-    storageBucket: 'spotifyx07.firebasestorage.app',
+  static FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['ANDROID_API_KEY'] ?? '',
+    appId: dotenv.env['ANDROID_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['ANDROID_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['ANDROID_PROJECT_ID'] ?? '',
+    storageBucket: dotenv.env['ANDROID_STORAGE_BUCKET'],
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCB5yRrjToyvIc3dsI6DEGJZNUdHj5xFCw',
-    appId: '1:99000635271:ios:8066c30da22a6d0a454d22',
-    messagingSenderId: '99000635271',
-    projectId: 'spotifyx07',
-    storageBucket: 'spotifyx07.firebasestorage.app',
-    iosBundleId: 'com.example.spotify',
+  static FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY'] ?? '',
+    appId: dotenv.env['IOS_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['IOS_PROJECT_ID'] ?? '',
+    storageBucket: dotenv.env['IOS_STORAGE_BUCKET'],
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID'],
   );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyCB5yRrjToyvIc3dsI6DEGJZNUdHj5xFCw',
-    appId: '1:99000635271:ios:8066c30da22a6d0a454d22',
-    messagingSenderId: '99000635271',
-    projectId: 'spotifyx07',
-    storageBucket: 'spotifyx07.firebasestorage.app',
-    iosBundleId: 'com.example.spotify',
+  static FirebaseOptions macos = FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY'] ?? '',
+    appId: dotenv.env['IOS_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['IOS_PROJECT_ID'] ?? '',
+    storageBucket: dotenv.env['IOS_STORAGE_BUCKET'],
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID'],
   );
 
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyCQYqJwyhsbRrfT2_0jghJNyenFVvnWqPI',
-    appId: '1:99000635271:web:78c48137939b58a4454d22',
-    messagingSenderId: '99000635271',
-    projectId: 'spotifyx07',
-    authDomain: 'spotifyx07.firebaseapp.com',
-    storageBucket: 'spotifyx07.firebasestorage.app',
+  static FirebaseOptions windows = FirebaseOptions(
+    apiKey: dotenv.env['WINDOWS_API_KEY'] ?? '',
+    appId: dotenv.env['WINDOWS_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['WINDOWS_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['WINDOWS_PROJECT_ID'] ?? '',
+    authDomain: dotenv.env['WINDOWS_AUTH_DOMAIN'],
+    storageBucket: dotenv.env['WINDOWS_STORAGE_BUCKET'],
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify/core/config/theme/app_theme.dart';
@@ -12,6 +13,7 @@ import 'package:spotify/service_locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
